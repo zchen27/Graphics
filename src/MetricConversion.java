2e360507adeeeb622df84fc5ac941bb3ac297967
zchen27 <zchen27@gmail.com>
2013-11-18 14:34:37 -0500

    Commit

--- src/MetricConversion.java
+++ src/MetricConversion.java
@@ -6,14 +6,8 @@ import javax.swing.*;
 public class MetricConversion
 {
 	final static String PROMPT = "SELECT A CONVERSION TYPE";
-	final static String IN_CM = "inches to centimeters";
-	final static String FT_M = "feet to meters";
-	final static String GL_L = "gallons to liters";
-	final static String LB_KG = "pounds to kilograms";
-	final static double ICFACTOR = 2.54;
-	final static double FMFACTOR = 0.3048;
-	final static double GLFACTOR = 4.5461;
-	final static double LKFACTOR = 0.4536;
+	final static String[] CHOICES = {"inches to centimeters", "feet to meters", "gallons to liters", "pounds to kilograms"};
+	final static double ICFACTOR = {2.54, 0.304, 4.5461,0.4536};
 	
 	JFrame frame;
 	JPanel panel;
@@ -28,6 +22,12 @@ public class MetricConversion
 		
 		panel = new JPanel();
 		panel.setLayout(new GridLayout(0, 1));
+		panel.setBorder(BorderFactory.createEmptyBorder(50, 50, 50, 50));
 		
+		prompt = new JLabel(PROMPT);
+		prompt.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+		panel.add(prompt);
+		
+		comboBox = new JComboBox(CHOICES);
 	}
 }
