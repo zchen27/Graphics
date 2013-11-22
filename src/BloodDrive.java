ed2cb32d3d1642af0e181b7e540c0a8ad49fa44e
zchen27 <zchen27@gmail.com>
2013-11-22 14:08:32 -0500

    Commit

---
+++ src/BloodDrive.java
@@ -0,0 +1,34 @@
+import java.awt.*;
+import java.awt.event.*;
+import java.math.*;
+import javax.swing.*;
+
+public class BloodDrive
+{
+	JFrame frame;
+	JPanel panel;
+	JLabel department0 = new JLabel("BALLISTICS AND ROCKETRY");
+	JLabel department1 = new JLabel("MATERIAL SCIENCE");
+	JLabel department2 = new JLabel("CIVIL ENGINEERING");
+	JLabel department3 = new JLabel("LEGAL");
+	JLabel department4 = new JLabel("EXOTIC MECHANICS");
+	JTextField text0;
+	JTextField text1;
+	JTextField text2;
+	JTextField text3;
+	JTextField text4;
+	
+	BigDecimal blood0;
+	BigDecimal blood1;
+	BigDecimal blood2;
+	BigDecimal blood3;
+	BigDecimal blood4;
+	
+	
+	public BloodDrive()
+	{
+		frame = new JFrame();
+		panel = new JPanel();
+		
+	}
+}
