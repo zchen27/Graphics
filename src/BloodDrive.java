082a52e1b889e892d2405a9e3c82782fc9eda56f
zchen27 <zchen27@gmail.com>
2013-11-26 09:46:03 -0500

    Commit

--- src/BloodDrive.java
+++ src/BloodDrive.java
@@ -3,15 +3,16 @@ import java.awt.event.*;
 import java.math.*;
 import javax.swing.*;
 
-public class BloodDrive
+public class BloodDrive implements ActionListener
 {
 	JFrame frame;
 	JPanel panel;
+	JPanel graph;
 	JLabel department0 = new JLabel("BALLISTICS AND ROCKETRY");
 	JLabel department1 = new JLabel("MATERIAL SCIENCE");
-	JLabel department2 = new JLabel("CIVIL ENGINEERING");
-	JLabel department3 = new JLabel("LEGAL");
-	JLabel department4 = new JLabel("EXOTIC MECHANICS");
+	JLabel department2 = new JLabel("STRUCTURAL SYSTEMS");
+	JLabel department3 = new JLabel("FORTIFICATIONS");
+	JLabel department4 = new JLabel("LOGISTICS");
 	JTextField text0;
 	JTextField text1;
 	JTextField text2;
@@ -24,6 +25,15 @@ public class BloodDrive
 	BigDecimal blood3;
 	BigDecimal blood4;
 	
+	private class Graph extends JPanel
+	{
+		@Override
+		public void paintComponent(Graphics g)
+		{
+			super.paintComponent(g);
+		}
+	}
+	
 	public BloodDrive()
 	{
 		frame = new JFrame();
@@ -31,5 +41,45 @@ public class BloodDrive
 		panel.setLayout(new GridLayout(0, 2));
 		panel.setBorder(BorderFactory.createEmptyBorder(50, 50, 50, 50));
 		
+		panel.add(department0);
+		
+		text0 = new JTextField();
+		text0.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+		text0.addActionListener(this);
+		panel.add(text0);
+		
+		panel.add(department1);
+		
+		text1 = new JTextField();
+		text1.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+		text1.addActionListener(this);
+		panel.add(text1);
+		
+		panel.add(department2);
+		
+		text2 = new JTextField();
+		text2.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+		text2.addActionListener(this);
+		panel.add(text2);
+		
+		panel.add(department3);
+		
+		text3 = new JTextField();
+		text3.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+		text3.addActionListener(this);
+		panel.add(text3);
+		
+		panel.add(department4);
+		
+		text4 = new JTextField();
+		text4.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+		text4.addActionListener(this);
+		panel.add(text4);
+	}
+	
+	@Override
+	public void actionPerformed(ActionEvent ae)
+	{
+		throw new UnsupportedOperationException("Not supported yet.");
 	}
 }
